8376aa781523ea5e53eba459628baf54a32922d0
Colin Timmermans <colintimmermans@gmail.com>
2012-04-17 17:46:24 +0300

    Extract image url erb template to separate file to make our internal tests pass

---
+++ utils/images.js
@@ -0,0 +1,12 @@
+//= require ./images_url
+
+Ember.mixin(Flame, {
+    image: function(imageUrl) {
+      if (typeof FlameImageUrlPrefix === 'undefined') {
+          return Flame.imagePath || '' + imageUrl;
+      } else {
+          return FlameImageUrlPrefix + imageUrl;
+      }
+    }
+});
+
