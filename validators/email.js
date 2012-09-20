3e00614236b31070764385dfda3e4a5377944893
Colin Timmermans <colintimmermans@gmail.com>
2012-09-20 09:50:06 +0300

    Allow local part in email address to end with '-'.
    Fixes #67

--- validators/email.js
+++ validators/email.js
@@ -1,6 +1,6 @@
 Flame.Validator.email = Flame.Validator.create({
     validate: function(target, key) {
-        var pattern = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i;
+        var pattern = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9\-]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i;
         var string = target.get(key);
         return pattern.test(string);
     }
