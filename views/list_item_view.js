d255ddf92e3660ab704277aef29666cf3c58c615
Parker Morse <flashesofpanic@gmail.com>
2012-10-17 07:22:30 -0400

    A little more attention to touches in ListItemView

--- views/list_item_view.js
+++ views/list_item_view.js
@@ -33,19 +33,29 @@ Flame.ListItemView = Flame.View.extend({
         }
     },
 
-    // Handle touch events by normalizing events and handing to mouse events
+    // We don't have the normalize function available, so we'll pass
+    // these events to the relevant parent view handlers.
     touchMove: function(evt) {
-        this.mouseMove(Flame.State.normalizeTouchEvents(evt));
-        return true;
+        if (this._parentViewOnMouseDown !== undefined) {
+            return this._parentViewOnMouseDown.touchMove(evt);
+        } else {
+            return false;
+        }
     },
 
     touchStart: function(evt) {
-        this.mouseDown(Flame.State.normalizeTouchEvents(evt));
-        return true;
+        // The same caveats apply as in mouseDown: store the parentView and hand the event up to it
+        this._parentViewOnMouseDown = this.get('parentView');
+        return this._parentViewOnMouseDown.mouseDownOnItem(this.get('contentIndex'), evt);
     },
 
-    touchEng: function(evt) {
-        this.mouseUp(Flame.State.normalizeTouchEvents(evt));
-        return true;
+    touchEnd: function(evt) {
+        if (this._parentViewOnMouseDown !== undefined) {
+            var parentView = this._parentViewOnMouseDown;
+            this._parentViewOnMouseDown = undefined;
+            return parentView.mouseUpOnItem(evt);
+        } else {
+            return false;
+        }
     }
 });
