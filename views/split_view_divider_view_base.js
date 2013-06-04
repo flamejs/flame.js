10b7941b88c0dc5a2fdd01aaa5f991ee9267c1d9
Jaakko Strömlin <jaakko.lofstrom@relex.fi>
2013-06-05 01:07:57 +0300

    Use any view as split view divider

---
+++ views/split_view_divider_view_base.js
@@ -0,0 +1,45 @@
+Flame.SplitViewDividerViewBase = Ember.Mixin.create(Flame.Statechart, {                                                         
+    classNames: 'flame-split-view-divider'.w(),
+    initialState: 'idle',
+
+    idle: Flame.State.extend({
+        mouseDown: function(event) {
+            var parentView = this.getPath('owner.parentView');
+            if (!parentView.get('allowResizing')) return false;
+            parentView.startResize(event);
+            this.gotoState('resizing');
+            return true;
+        },
+
+        touchStart: function(event) {
+            // Normalize the event and send it to mouseDown
+            this.mouseDown(this.normalizeTouchEvents(event));
+            return true;
+        },
+
+        doubleClick: function(event) {
+            var parentView = this.getPath('owner.parentView');
+            if (!parentView.get('allowResizing')) return false;
+            parentView.toggleCollapse(event);
+            return true;
+        }
+    }),
+
+    resizing: Flame.State.extend({
+        mouseMove: function(event) {
+            this.getPath('owner.parentView').resize(event);
+            return true;
+        },
+
+        touchMove: function(event) {
+            // Don't scroll the page while we're doing this
+            event.preventDefault();
+            // Normalize the event and send it to mouseDown
+            this.mouseMove(this.normalizeTouchEvents(event));
+            return true;
+        },
+
+        mouseUp: Flame.State.gotoHandler('idle'),
+        touchEnd: Flame.State.gotoHandler('idle')
+    })
+});
