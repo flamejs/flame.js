c07c2cbe5e5009b06594e7899eb1562fb7b69dc3
Colin Timmermans <colintimmermans@gmail.com>
2014-02-24 13:00:54 +0100

    Use a computed property instead of a binding in FormView.

--- views/form_view.js
+++ views/form_view.js
@@ -226,7 +226,7 @@ Flame.FormView = Flame.View.extend({
         var object = this.get('object');
         var settings = {
             layout: { topPadding: 1, bottomPadding: 1, width: this.get('controlWidth') },
-            valueBinding: 'parentView.parentView.object.%@'.fmt(property),
+            value: Ember.computed.alias('parentView.parentView.object.%@'.fmt(property)),
             isValid: Ember.computed.notEqual('parentView.parentView.object.%@IsValid'.fmt(property), false),
             isDisabled: descriptor.isDisabled ? descriptor.isDisabled : Ember.computed.equal('parentView.parentView.object.%@IsDisabled'.fmt(property), true)
         };
