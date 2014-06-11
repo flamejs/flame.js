45b2bf3393debc6940a926ac0cafacbd5e6dcbeb
Tomi Laine <tomi.laine@relex.fi>
2014-06-11 15:31:00 +0300

    separate buttonview and messageview

--- views/alert_panel.js
+++ views/alert_panel.js
@@ -9,6 +9,68 @@ Flame.AlertPanel.INFO_ICON = Flame.image('info_icon.png');
 Flame.AlertPanel.WARN_ICON = Flame.image('warn_icon.png');
 Flame.AlertPanel.ERROR_ICON = Flame.image('error_icon.png');
 
+
+Flame.AlertPanelButtonView = Flame.View.extend({
+    layout: { width: '100%', right: 0, height: 30 },
+    childViews: 'cancelButtonView confirmButtonView'.w(),
+    cancelButtonTitle: 'Cancel',
+    confirmButtonTitle: 'OK',
+    isCancelVisible: true,
+    isConfirmVisible: true,
+    isCancelDisabled: false,
+    isConfirmDisabled: false,
+    alertPanelView: null,
+
+    cancelButtonView: Flame.ButtonView.extend({
+        layout: { width: 90, bottom: 2, right: 110 },
+        title: Ember.computed.alias('parentView.cancelButtonTitle'),
+        isVisible: Ember.computed.alias('parentView.isCancelVisible'),
+        isDisabled: Ember.computed.alias('parentView.isCancelDisabled'),
+        action: function() {
+            this.get('parentView.alertPanelView').onCancel();
+        }
+    }),
+
+    confirmButtonView: Flame.ButtonView.extend({
+        layout: { width: 90, bottom: 2, right: 2 },
+        title: Ember.computed.alias('parentView.confirmButtonTitle'),
+        isVisible: Ember.computed.alias('parentView.isConfirmVisible'),
+        isDisabled: Ember.computed.alias('parentView.isConfirmDisabled'),
+        isDefault: true,
+        action: function() {
+            this.get('parentView.alertPanelView').onConfirm();
+        }
+    })
+});
+
+Flame.AlertPanelMessageView = Flame.View.extend({
+    layout: { left: 10, right: 2, height: 'measuredHeight'},
+    childViews: 'iconView messageView'.w(),
+    messageViewHeight: 0,
+    measuredHeight: function() {
+        var width  = "width: %@px;".fmt(this.get('messageViewHeight'));
+        var parentClasses = this.get('parentView.parentView.classNames').join(' ');
+        var elementClasses = this.get('messageView.classNames').join(' ');
+        var computedMessageViewHeight = Flame.measureString(this.get('message'), parentClasses, elementClasses, width).height;
+        return Math.max(Math.min(computedMessageViewHeight, 600), 50);
+    }.property('message', 'messageViewHeight'),
+    message: null,
+
+    iconView: Flame.ImageView.extend({
+        layout: { left: 10 },
+        value: Flame.computed.nearest('icon')
+    }),
+
+    messageView: Flame.LabelView.extend({
+        layout: { left: 75, right: 2, height: null },
+        didInsertElement: function() {
+            this.set('parentView.messageViewHeight', this.$().width());
+        },
+        allowWrapping: true,
+        value: Ember.computed.alias('parentView.message')
+    })
+});
+
 Flame.AlertPanel.reopen({
     layout: { centerX: 0, centerY: -50, width: 400 },
 
@@ -23,40 +85,24 @@ Flame.AlertPanel.reopen({
     isCloseable: true,
     title: '',
     message: '',
-    cancelButtonTitle: 'Cancel',
     confirmButtonTitle: 'OK',
+    cancelButtonTitle: 'Cancel',
 
     contentView: Flame.View.extend({
         layout: { left: 15, right: 15, top: 36, bottom: 15 },
         layoutManager: Flame.VerticalStackLayoutManager.create({ topMargin: 20, bottomMargin: 20, spacing: 10 }),
-        childViews: 'alertView confirmView'.w(),
+        childViews: 'messageView buttonView'.w(),
 
-        alertView: Flame.AlertPanelAlertView,
-
-        confirmView: Flame.View.extend({
-            layout: { width: 200, right: 0, height: 30 },
-            childViews: 'cancelButtonView okButtonView'.w(),
-
-            cancelButtonView: Flame.ButtonView.extend({
-                layout: { width: 90, bottom: 2, right: 110 },
-                titleBinding: '^cancelButtonTitle',
-                isVisibleBinding: '^isCancelVisible',
-                action: function() {
-                    this.get('parentView.parentView.parentView').onCancel();
-                }
-            }),
-
-            okButtonView: Flame.ButtonView.extend({
-                layout: { width: 90, bottom: 2, right: 2 },
-                titleBinding: '^confirmButtonTitle',
-                isVisibleBinding: '^isConfirmVisible',
-                isDefault: true,
-                action: function() {
-                    this.get('parentView.parentView.parentView').onConfirm();
-                }
-            })
+        messageView: Flame.AlertPanelMessageView.extend({
+            message: Flame.computed.nearest('message'),
         }),
-
+        buttonView: Flame.AlertPanelButtonView.extend({
+            confirmButtonTitle: Flame.computed.nearest('confirmButtonTitle'),
+            cancelButtonTitle: Flame.computed.nearest('cancelButtonTitle'),
+            alertPanelView: Ember.computed.alias('parentView.parentView'),
+            isCancelVisible: Flame.computed.nearest('isCancelVisible'),
+            isConfirmVisible: Flame.computed.nearest('isConfirmVisible')
+        })
     }),
 
     // Key event handler for ESC
@@ -73,34 +119,6 @@ Flame.AlertPanel.reopen({
     onCancel: function() {
         if (this.get('isCloseable')) this.close();
     }
-});
-
-Flame.AlertPanelAlertView = Flame.View.extend({
-    layout: { left: 10, right: 2, height: 'measuredHeight'},
-    childViews: 'iconView messageView'.w(),
-    messageViewHeight: 0,
-    measuredHeight: function() {
-        var width  = "width: %@px;".fmt(this.get('messageViewHeight'));
-        var parentClasses = this.get('parentView.parentView.classNames').join(' ');
-        var elementClasses = this.get('messageView.classNames').join(' ');
-        var computedMessageViewHeight = Flame.measureString(this.get('message'), parentClasses, elementClasses, width).height;
-        return Math.max(Math.min(computedMessageViewHeight, 600), 50);
-    }.property('message', 'messageViewHeight'),
-    message: Flame.computed.nearest('message'),
-
-    iconView: Flame.ImageView.extend({
-        layout: { left: 10 },
-        value: Flame.computed.nearest('icon')
-    }),
-
-    messageView: Flame.LabelView.extend({
-        layout: { left: 75, right: 2, height: null },
-        didInsertElement: function() {
-            this.set('parentView.messageViewHeight', this.$().width());
-        },
-        allowWrapping: true,
-        value: Ember.computed.alias('parentView.message')
-    }),
 });
 
 Flame.AlertPanel.reopenClass({
