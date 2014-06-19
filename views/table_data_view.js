fd25b5b5e31c11be2df502837975d5915938e2df
Colin Timmermans <colintimmermans@gmail.com>
2014-06-19 10:49:07 +0200

    Don't set selected cell to null before changing state.

--- views/table_data_view.js
+++ views/table_data_view.js
@@ -537,12 +537,10 @@ Flame.TableDataView = Flame.View.extend(Flame.Statechart, {
         exitState: function() {
             var owner = this.get('owner');
             var editField = owner.get('editField');
-
-            var selectedCell = owner.get('selectedCell');
-            selectedCell.removeClass('editing');
-
             editField.hide();
             editField.removeClass('invalid');
+
+            owner.get('selectedCell').removeClass('editing');
         }
     }),
 
@@ -554,11 +552,14 @@ Flame.TableDataView = Flame.View.extend(Flame.Statechart, {
     },
 
     willLoseKeyResponder: function() {
+        // We first change the state so that the current state can do any
+        // required cleanup before we set the selected cell to null.
+        this.gotoFlameState('loaded');
+
         this.setProperties({
             selectedCell: null,
             selectionEnd: null
         });
-        this.gotoFlameState('loaded');
     },
 
     columnIndex: function(cell) {
