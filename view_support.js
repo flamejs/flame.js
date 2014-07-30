752119939157394a34d146612193af34ac35742e
Colin Timmermans <colintimmermans@gmail.com>
2014-07-30 11:23:40 +0200

    Don't leave observers hanging around.

--- view_support.js
+++ view_support.js
@@ -8,7 +8,8 @@ Flame.ViewSupport = {
 
         // Add observers for displayProperties so that the view gets rerendered if any of them changes
         var properties = this.get('displayProperties') || [];
-        for (var i = 0; i < properties.length; i++) {
+        var length = properties.length;
+        for (var i = 0; i < length; i++) {
             var property = properties[i];
             this.addObserver(property, this, this.rerender);
         }
@@ -23,6 +24,14 @@ Flame.ViewSupport = {
         }
     },
 
+    willDestroy: function() {
+        var properties = this.get('displayProperties') || [];
+        var length = properties.length;
+        for (var i = 0; i < length; i++) {
+            this.removeObserver(properties[i]);
+        }
+    },
+
     createChildView: function(view, attrs) {
         view = this._super(view, attrs);
         if (view instanceof Ember.View && Flame._bindPrefixedBindings(view)) {
@@ -32,10 +41,10 @@ Flame.ViewSupport = {
     },
 
     /**
-     Collects the classNames that were defined in super classes, but not
-     classNames in Flame.View or superclasses that are above it in the
-     class hierarchy.
-     */
+      Collects the classNames that were defined in super classes, but not
+      classNames in Flame.View or superclasses that are above it in the
+      class hierarchy.
+    */
     _collectSuperClassNames: function() {
         var superClassNames = [];
         var superClass = Object.getPrototypeOf(Object.getPrototypeOf(this));
