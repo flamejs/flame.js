284f3f1d2e7769ae6f1e30d03ca64cd3dedad7c9
Colin Timmermans <colintimmermans@gmail.com>
2014-08-15 00:41:29 +0200

    Don't depend on String prototype extension.

--- validators/not_blank.js
+++ validators/not_blank.js
@@ -2,7 +2,7 @@ Flame.Validator.notBlank = Flame.Validator.create({
     validate: function(target, key) {
         var string = target.get(key);
         if (string) {
-            return !string.toString().isBlank();
+            return string.trim().length !== 0;
         } else {
             return false;
         }
