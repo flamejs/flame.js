881a7694d69411323a16e30d44287e7d21f89f09
Tomster <tomster@emberjs.com>
2014-08-20 08:43:45 +0200

    Use Ember.isBlank is notBlank validator.

--- validators/not_blank.js
+++ validators/not_blank.js
@@ -1,10 +1,5 @@
 Flame.Validator.notBlank = Flame.Validator.create({
     validate: function(target, key) {
-        var string = target.get(key);
-        if (string) {
-            return string.trim().length !== 0;
-        } else {
-            return false;
-        }
+        return !Ember.isBlank(target.get(key));
     }
 });
