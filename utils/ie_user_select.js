2d501015d41fdeb096934233681320235f3f85b1
Colin Timmermans <colintimmermans@gmail.com>
2014-09-10 10:51:42 +0200

    Remove dependency on jquery-migrate.

--- utils/ie_user_select.js
+++ utils/ie_user_select.js
@@ -1,5 +1,5 @@
 // IE < 10 doesn't support -ms-user-select CSS property, so we need to use onselectstart event to stop the selection
-if (Ember.$.browser.msie && Ember.$.browser.version < 10) {
+if (/msie 9/i.test(window.navigator.userAgent)) {
     Ember.$(function() {
         Ember.$('body').on('selectstart', function(e) {
             var target = Ember.$(e.target);
