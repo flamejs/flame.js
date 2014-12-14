2952a69a9cb87ccb6db5652d1e768618b3260ba0
Colin Timmermans <colintimmermans@gmail.com>
2014-12-14 16:03:41 +0100

    Correctly use run loop in ProgressView.

--- views/progress_view.js
+++ views/progress_view.js
@@ -1,5 +1,8 @@
 Flame.ProgressView = Flame.View.extend({
     classNames: ['flame-progress-view'],
+
+    value: null,
+    maximum: null,
     animate: false,
 
     handlebars: function() {
@@ -16,13 +19,10 @@ Flame.ProgressView = Flame.View.extend({
             if (progress > 1) progress = 1;
             return Math.floor(width * progress) - 4;
         }
-    }.property('value', 'maximum').volatile(),
+    }.property('value', 'maximum'),
 
-    _sizeDidChange: function() {
-        // In CubeTableLoadingView, the progress views are rendered before the value & maximum bindings have synchronized,
-        // which means the handlebars template uses width 0. Then they synchronize _before_ the element is added to DOM,
-        // which means $(...) doesn't work yet. Defer updating to next runloop.
-        Ember.run.next(this, function() {
+    sizeDidChange: function() {
+        Ember.run.schedule('afterRender', this, function() {
             if (this.get('animate')) {
                 this.$('.progress-bar').animate({ width: this.get('size') }, 300);
             } else {
