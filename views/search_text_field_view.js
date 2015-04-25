9d881bde43756ece9722ce829c801c1f0d79bf70
Colin Timmermans <colintimmermans@gmail.com>
2015-04-25 10:25:43 +0200

    Add clear button to SearchTextField.

--- views/search_text_field_view.js
+++ views/search_text_field_view.js
@@ -2,6 +2,18 @@
 
 Flame.SearchTextFieldView = Flame.TextFieldView.extend({
     classNames: ['flame-search-field'],
+    childViews: ['textField', 'clearButton'],
+
+    clearButton: Flame.ButtonView.extend({
+        resetClassNames: true,
+        layout: { right: 5, top: 2 },
+        isVisible: Ember.computed.bool('parentView.value'),
+        handlebars: '<img src="/assets/images/search_clear.svg">',
+
+        action: function() {
+            this.get('parentView').cancel();
+        }
+    }),
 
     cancel: function() {
         if (Ember.isEmpty(this.get('value'))) {
