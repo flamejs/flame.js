50fcd5224250c1f18af270e666f30aa4dbab54ce
mattijauhiainen <mattija@Mattis-MacBook-Pro.local>
2015-10-07 15:29:37 +0800

    Add image height and width attributes to image view

--- views/image_view.js
+++ views/image_view.js
@@ -1,3 +1,7 @@
 Flame.ImageView = Flame.View.extend({
-    handlebars: '<img {{bind-attr src="view.value"}}>'
+    imageHeight: null,
+    imageWidth: null,
+    value: null,
+
+    handlebars: '<img {{bind-attr src="view.value"}} {{bind-attr height="view.imageHeight"}} {{bind-attr width="view.imageWidth"}}>'
 });
