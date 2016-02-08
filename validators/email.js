0b6aea8ff6db184c1275e6d9933a07aed17ee16b
jmalves <joao.alves@relexsolutions.com>
2016-02-08 15:21:51 +0200

    Increased valid email toplevel domain length

--- validators/email.js
+++ validators/email.js
@@ -1,4 +1,4 @@
-var pattern = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9\-]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i;
+var pattern = /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9\-]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,63}$/i;
 
 Flame.Validator.email = Flame.Validator.create({
     validate: function(target, key) {
